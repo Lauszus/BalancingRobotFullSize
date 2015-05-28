6111307ff2d4c2a25e798fde9dd42dce1d1ef7d1
Kristian Sloth Lauszus <lauszus@gmail.com>
2015-05-28 21:44:25 +0200

    Push back a little more when driving

--- PID.cpp
+++ PID.cpp
@@ -40,7 +40,7 @@ void updatePID(double restAngle, double offset, double turning, double dt) {
   lastError = error;
   PIDValue = pTerm + iTerm + dTerm;
 
-  currentSpeed = (currentSpeed + PIDValue * 0.004) * 0.999; // TODO: Explain this
+  currentSpeed = (currentSpeed + PIDValue * 0.005) * 0.999; // TODO: Explain this
   //currentSpeed = constrain(currentSpeed, -50, 50);
 
   //Serial.print(PIDValue); Serial.write('\t'); Serial.print(currentSpeed); Serial.write('\t');
